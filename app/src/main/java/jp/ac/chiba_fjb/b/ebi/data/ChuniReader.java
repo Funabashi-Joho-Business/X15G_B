c1d6890fb77080d444a36a9748964f2fd5b3783b
WIN_NT\x15g008 <x15g008@chiba-fjb.ac.jp>
2017-10-11 11:52:48 +0900

    10/11

--- app/src/main/java/jp/ac/chiba_fjb/b/ebi/data/ChuniReader.java
+++ app/src/main/java/jp/ac/chiba_fjb/b/ebi/data/ChuniReader.java
@@ -7,25 +7,31 @@ import java.util.regex.Pattern;
  * Created by oikawa on 2017/10/02.
  */
 
+
 public class ChuniReader {
-	public static boolean recvTest(){
-		String adr = "https://weather.yahoo.co.jp/weather/jp/12/4510.html";
-		String content = HttpReader.getContent(adr);
-		if(content == null)
-			return false;
+    public static boolean recvTest(){
+        int i=15;
+        String adr = "https://chunithm.sega.jp/player/music/pops_anime.html";
+        String content = HttpReader.getContent(adr);
+        if(content == null)
+            return false;
 
-		//検索パターンを設定
-		Pattern p = Pattern.compile(
-			"<p class=\"date\">(.*?)</p>.*?"+
-			"<img src=\".*?\" border=\"0\" alt=\"(.*?)\">.*?"+
-			"<li class=\"high\"><em>(.*?)</em>.*?"+
-			"<li class=\"low\"><em>(.*?)</em>",Pattern.DOTALL);
-		Matcher m = p.matcher(content);
-		while(m.find()){
-			System.out.format("日付:%s 天気:%s 最高気温:%s 最低気温:%s\n",
-				m.group(1),m.group(2),m.group(3),m.group(4));
-		}
-		return true;
+        //検索パターンを設定  .*?ワイルドカード
+        Pattern p = Pattern.compile(
+                "<h3 class=\"musiclist__item__right__name\">(.*?)</h3>.*?"+
+                      "<dl class=\"musiclist__item__right__artist\">(.*?)</dl>.*?"+
+                        "<img src=\"(.*?)\">.*?"+
+                        "<div class=\"musiclist__item__level__box__num musiclist__item__level__box__num-1\">(.*?)</div>.*?"+
+                        "<div class=\"musiclist__item__level__box__num musiclist__item__level__box__num-2\">(.*?)</div>.*?"+
+                        "<div class=\"musiclist__item__level__box__num musiclist__item__level__box__num-3\">(.*?)</div>.*?"+
+                        "<div class=\"musiclist__item__level__box__num musiclist__item__level__box__num-4\">(.*?)</div>",Pattern.DOTALL);
+//                          "<div class=\"musiclist__item musiclist__item-pops_anime is-active is-secret\"id=\"music15\".*?>(.*?)</div>",Pattern.DOTALL);
 
-	}
+        Matcher m = p.matcher(content);
+            while(m.find()){
+          System.out.format("1:%s\n 2:%s\n 3:%s\n 4:%s %s %s %s",m.group(1),m.group(2),m.group(3),m.group(4),m.group(5),m.group(6),m.group(7));
+        }
+        return true;
+
+    }
 }
